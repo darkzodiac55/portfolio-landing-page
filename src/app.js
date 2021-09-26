e9d662ada8aaab5f2ec85a621f36640ce2d52bf9
Igor G <darkzodiac55@gmail.com>
2021-09-27 00:21:04 +0200

    react project added

--- src/app.js
+++ src/app.js
@@ -4,10 +4,11 @@ import "./app.css"
 
 let btn = document.querySelector('.list p:nth-child(2)')
 let main = document.querySelector('body').style
-let proj1 = document.querySelector('.cont:nth-of-type(1)')
-let proj2 = document.querySelector('.cont:nth-of-type(2)')
+let proj1 = document.querySelector('#cont1')
+let proj2 = document.querySelector('#cont2')
 let proj3 = document.querySelector('#cont3')
 let proj4 = document.querySelector('#cont4')
+let proj5 = document.querySelector('#cont5')
 
 let wait = 0
 
@@ -53,11 +54,13 @@ const red = changeClr('linear-gradient(90deg, rgba(250,231,208,1) 3%, rgba(255,2
 const green = changeClr('linear-gradient(to left, #FFFFFF, #6DD5FA, #2980B9)')
 const proj3bg = changeClr('linear-gradient(to left, #dc2430, #7b4397)')
 const proj4bg = changeClr('linear-gradient(to left, #19547b, #ffd89b)')
+const proj5bg = changeClr('linear-gradient(to top, #c9d6ff, #e2e2e2)')
 
 proj1.addEventListener('mouseenter', green)
 proj2.addEventListener('mouseenter', red)
 proj3.addEventListener('mouseenter', proj3bg)
 proj4.addEventListener('mouseenter', proj4bg)
+proj5.addEventListener('mouseenter', proj5bg)
 
 
 
